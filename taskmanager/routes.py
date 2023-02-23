8e5d4da138844f3cfef079f9befb07fd92f394b3
rkyzk <reikoyazaki@yahoo.com>
2023-02-23 09:17:10 +0000

    remove redirect to home func after editing tasks and stay on edit page

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -63,6 +63,7 @@ def add_task():
 @app.route("/edit_task<int:task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
     task = Task.query.get_or_404(task_id)
+    categories = list(Category.query.order_by(Category.category_name).all())
     if request.method == "POST":
         task.task_name = request.form.get("task_name")
         task.task_description = request.form.get("task_description")
@@ -70,8 +71,7 @@ def edit_task(task_id):
         task.due_date = request.form.get("due_date")
         task.category_id = request.form.get("category_id")
         db.session.commit()
-        return redirect(url_for("home"))
-    return render_template("edit_task.html", task=task)
+    return render_template("edit_task.html", categories=categories, task=task)
 
 
 @app.route("/delete_task<int:task_id>", methods=["GET", "POST"])
